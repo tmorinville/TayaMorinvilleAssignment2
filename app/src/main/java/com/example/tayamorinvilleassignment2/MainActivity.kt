2c83651deb73d6953a3b8404538208da59d7e972
Taya Morinville <149643813+tmorinville@users.noreply.github.com>
2024-09-13 09:06:29 -0500

    Initial commit, uploading completed project

---
+++ app/src/main/java/com/example/tayamorinvilleassignment2/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.tayamorinvilleassignment2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Explicit Intent
+        val explicitButton = findViewById<Button>(R.id.explicitBtn)
+        explicitButton.setOnClickListener{
+            val explicitIntent = Intent(this, SecondActivity::class.java)
+            startActivity(explicitIntent)
+        }
+
+        // Implicit Intent
+        val implicitButton = findViewById<Button>(R.id.implicitBtn)
+        implicitButton.setOnClickListener{
+            // Intent with action matching the action specified for second activity in AndroidManifest.xml
+            val implicitIntent = Intent("android.intent.action.SECONDACTIVITY")
+            startActivity(implicitIntent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tayamorinvilleassignment2/SecondActivity.kt
@@ -0,0 +1,29 @@
+package com.example.tayamorinvilleassignment2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Back to main activity button
+        val backToMainButton = findViewById<Button>(R.id.backToMainActivityBtn)
+        backToMainButton.setOnClickListener{
+            val backToMainIntent = Intent(this, MainActivity::class.java)
+            startActivity(backToMainIntent)
+        }
+    }
+}
