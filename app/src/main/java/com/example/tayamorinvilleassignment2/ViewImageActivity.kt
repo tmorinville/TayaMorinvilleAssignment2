62955b0aa907773ad0c825dbe6047db46832897e
Taya Morinville <149643813+tmorinville@users.noreply.github.com>
2024-10-09 16:15:55 -0500

    Cleaned up ViewImageActivity.kt

--- app/src/main/java/com/example/tayamorinvilleassignment2/ViewImageActivity.kt
+++ app/src/main/java/com/example/tayamorinvilleassignment2/ViewImageActivity.kt
@@ -1,23 +1,5 @@
 package com.example.tayamorinvilleassignment2
 
-//import android.os.Bundle
-//import androidx.activity.enableEdgeToEdge
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.view.ViewCompat
-//import androidx.core.view.WindowInsetsCompat
-//
-//class ViewImageActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_view_image)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//    }
-//}
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
