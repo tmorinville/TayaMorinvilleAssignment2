78a20dd7e17b13dec73000bd8cc09d270ba6ad5b
Taya Morinville <149643813+tmorinville@users.noreply.github.com>
2024-11-19 17:48:57 -0600

    Completed permissions

--- app/src/main/java/com/example/tayamorinvilleassignment2/MainActivity.kt
+++ app/src/main/java/com/example/tayamorinvilleassignment2/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.tayamorinvilleassignment2
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -22,16 +25,26 @@ class MainActivity : AppCompatActivity() {
         // Explicit Intent
         val explicitButton = findViewById<Button>(R.id.explicitBtn)
         explicitButton.setOnClickListener{
-            val explicitIntent = Intent(this, SecondActivity::class.java)
-            startActivity(explicitIntent)
+            if(ContextCompat.checkSelfPermission(this, "com.example.tayamorinvilleassignment2.MSE412") != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, arrayOf("com.example.tayamorinvilleassignment2.MSE412"), 100)
+            }
+            else {
+                val explicitIntent = Intent(this, SecondActivity::class.java)
+                startActivity(explicitIntent)
+            }
         }
 
         // Implicit Intent
         val implicitButton = findViewById<Button>(R.id.implicitBtn)
         implicitButton.setOnClickListener{
             // Intent with action matching the action specified for second activity in AndroidManifest.xml
-            val implicitIntent = Intent("android.intent.action.SECONDACTIVITY")
-            startActivity(implicitIntent)
+            if(ContextCompat.checkSelfPermission(this, "com.example.tayamorinvilleassignment2.MSE412") != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, arrayOf("com.example.tayamorinvilleassignment2.MSE412"), 100)
+            }
+            else{
+                val implicitIntent = Intent("android.intent.action.SECONDACTIVITY")
+                startActivity(implicitIntent)
+            }
         }
 
         // Image Activity Intent
